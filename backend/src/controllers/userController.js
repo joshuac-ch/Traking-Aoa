1dfd9ab92fa0d84e5dfa9cb5d808892205c50235
joshua <condorenajoshua98@gmail.com>
2025-05-06 19:51:59 -0500

    Se creo la funccion insercion de usuarios

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,13 +1,38 @@
 const Usuarios=require("../../models/usuario")
 const GetUser=async(req,res)=>{
     try{
-       const modelo=Usuarios.findAll()
+       const modelo=await Usuarios.findAll()
        if(!modelo){
-        res.status(404).json({message:'No existe esta tabla'})
+        return res.status(404).json({message:'No existe esta tabla'})
        }
        res.status(200).json(modelo)
     }catch(err){
         console.error("hubo un error",err)
     }
 }
-module.exports={GetUser}
+const InsertUser=async(req,res)=>{
+    try{
+        //const {nombre,apellido,correo,telefono}=req.body
+        //if(!nombre,!apellido,!correo,!telefono){
+        //    res.status(404).json({message:"Hubo un error no pueden estar vacios los campos"})
+       // }
+        const nombre='nino'
+        const apellido='nakano'
+        const correo='nino@gmail.com'
+        const telefono=910320803
+        const modelo=await Usuarios.create({
+            nombre:nombre,
+            apellido:apellido,
+            correo:correo,
+            telefono:telefono
+        })
+        if(!modelo){
+            return res.status(404).json({message:"este modelo no existe"})
+        }
+        res.status(200).json({message:"se creo exitosamente el modelo"})
+        
+    }catch(err){
+        console.error("hubo un error",err)
+    }
+}
+module.exports={GetUser,InsertUser}
