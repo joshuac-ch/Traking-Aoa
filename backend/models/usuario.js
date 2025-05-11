a9709013ef65e5b8a1ec724d55a52ff71f0f9bcb
joshua <condorenajoshua98@gmail.com>
2025-05-10 20:14:41 -0500

    Se agrego el modelo usuario y el controllador mejorado

--- backend/models/usuario.js
+++ backend/models/usuario.js
@@ -9,15 +9,28 @@ const sequelize=require('../src/config/database')
      */
     static associate(models) {
       // define association here
+      usuario.hasMany(models.habitos,{foreignKey:'usuario_id'})
+      usuario.hasMany(models.metas,{foreignKey:'usuario_id'})
+      usuario.hasMany(models.actividades_diarias,{foreignKey:'usuario_id'})
+      usuario.hasMany(models.emociones,{foreignKey:"usuario_id"})
     }
   }
   usuario.init({
+    id:{
+     type:DataTypes.BIGINT,
+     allowNull:false,
+     primaryKey:true,
+     autoIncrement:true
+    },
     nombre:DataTypes.STRING,
     apellido: DataTypes.STRING,
     correo: DataTypes.STRING,
-    telefono: DataTypes.STRING
+    telefono: DataTypes.STRING,
+    pass:DataTypes.STRING,
   }, {
     sequelize,
     modelName: 'usuario',
+    timestamps:false,
+    freezeTableName:true
   });
 module.exports=usuario  
--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -12,16 +12,18 @@ const GetUser=async(req,res)=>{
 }
 const InsertUser=async(req,res)=>{
     try{
-        //const {nombre,apellido,correo,telefono}=req.body
-        //if(!nombre,!apellido,!correo,!telefono){
-        //    res.status(404).json({message:"Hubo un error no pueden estar vacios los campos"})
-       // }
-        const nombre='nino'
-        const apellido='nakano'
-        const correo='nino@gmail.com'
-        const telefono=910320803
+        const {nombre,apellido,correo,telefono}=req.body
+        if(!nombre,!apellido,!correo,!telefono){
+           return res.status(404).json({message:"Hubo un error no pueden estar vacios los campos"})
+        }
+        //const nombre='nino'
+        //const apellido='nakano'
+        //const correo='nino@gmail.com'
+        //const pass="nino"
+        //const telefono=910320803
         const modelo=await Usuarios.create({
             nombre:nombre,
+            pass:pass,
             apellido:apellido,
             correo:correo,
             telefono:telefono
@@ -35,4 +37,40 @@ const InsertUser=async(req,res)=>{
         console.error("hubo un error",err)
     }
 }
-module.exports={GetUser,InsertUser}
+const ShowUser=async(req,res)=>{
+    const {id}=req.params
+    const modelo=await Usuarios.findByPk(id)
+    if(!modelo){
+        return res.status(404).json({message:"No se encontro ese id"})
+    }
+    res.status(201).json(modelo)
+}
+const UpdateUser=async(req,res)=>{
+    try{
+        const {id}=req.params
+        const {nombre,apellido,correo,telefono}=req.body    
+        const modelo=await Usuarios.findOne({where:{id}})
+        if(!modelo){
+            return res.status(404).json({message:"No se encontro ese user"})
+        }
+        await modelo.update({
+        nombre,
+        apellido,
+        correo,
+        pass,
+        telefono:10320803
+        })
+        res.status(200).json(modelo)
+    }catch(err){
+        console.error("Hubo un error")
+    }
+}
+const DeleteUser=async(req,res)=>{
+    const {id}=req.params
+    const modelo=await Usuarios.destroy({where:{id}})
+    if(!modelo){
+        return res.status.json({message:"No se encontro ese id"})
+    }
+    res.status(200).json({message:"Se elimino correctamente"})
+}
+module.exports={GetUser,InsertUser,ShowUser,DeleteUser,UpdateUser}
