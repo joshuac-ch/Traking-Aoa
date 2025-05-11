369392f36457b90baf3672bee5ee8db5d4018823
joshua <condorenajoshua98@gmail.com>
2025-05-10 20:13:09 -0500

    Se agregaron los componentes para la creacion del login

--- my-app/components/app.js
+++ my-app/components/app.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import "../global.css"
-import { Text } from 'react-native'
+import { Button, Pressable, Text, TextInput, View } from 'react-native'
 export default function AppPrincipal() {
   return (
    <>
-    <Text className="text-4xl font-black">my app</Text>
-  
+    <Text className="text-4xl text-center font-black">Login</Text>
+    <View className='m-4'>
+        <Text>Correo</Text>
+        <TextInput className='rounded w-full bg-gray border-2' placeholder='example@gmail.com'></TextInput>
+    </View>
+    <View className=' m-4'>
+        <Text>Password</Text>
+        <TextInput className='rounded w-full bg-gray border-2' placeholder='*********'></TextInput>
+    </View>
+    <View className='flex-row justify-center p-4'>
+        <Pressable className='bg-blue-500  rounded-full p-4'>
+            <Text className='color-white text-lg '>Enviar datos</Text>
+        </Pressable>
+    </View>
    </>
 )}
