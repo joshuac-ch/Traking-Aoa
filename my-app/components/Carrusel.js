0389c9ec1e4ac14efb92564f47096bdbedeff054
joshua <condorenajoshua98@gmail.com>
2025-05-12 19:55:57 -0500

    Se agrego el componente carrusel

---
+++ my-app/components/Carrusel.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import PagerView from 'react-native-pager-view';
+export default function Carrusel() {
+ return (
+    
+    <View style={styles.container}>
+      <PagerView style={styles.container} initialPage={0}>
+        <View style={styles.page} key="1">
+          <Text>First page</Text>
+          <Text>Swipe ➡️</Text>
+        </View>
+        <View style={styles.page} key="2">
+          <Text>Second page</Text>
+        </View>
+        <View style={styles.page} key="3">
+          <Text>Third page</Text>
+        </View>
+      </PagerView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  page: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
