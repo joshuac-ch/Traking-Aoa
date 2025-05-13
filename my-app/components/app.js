0d500184e4f5a5d310ec71363520b1280e785c27
joshua <condorenajoshua98@gmail.com>
2025-05-13 18:55:58 -0500

    Se mejoro el componenete app

--- my-app/components/app.js
+++ my-app/components/app.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import "../global.css"
-import { Button, Pressable, Text, TextInput, View } from 'react-native'
+import {Pressable, StatusBar, Text, TextInput, View } from 'react-native'
+import usuarios from '../hooks/usuarios'
+import { useRouter } from 'expo-router'
+import { useUser } from './UserContext'
 export default function AppPrincipal() {
+    const {setUser}=useUser()
+  const {FectUsuarios,dataUser}=usuarios()
+    useEffect(()=>{
+        FectUsuarios()
+    },[])
+    const [correo, setcorreo] = useState('itsuki@gmail.com')
+    const [pass, setpass] = useState('123')
+    const navegar=useRouter()
+    const VeificarUse=()=>{
+        const datos=dataUser.find((d)=>d.correo==correo && d.pass==pass)
+        if(datos){
+            setUser(datos)
+            navegar.push("/Panel")
+        }else{
+            alert("error")
+        }
+    }
   return (
    <>
+   
     <Text className="text-4xl text-center font-black">Login</Text>
     <View className='m-4'>
-        <Text>Correo</Text>
-        <TextInput className='rounded w-full bg-gray border-2' placeholder='example@gmail.com'></TextInput>
+        <Text >Correo</Text>
+        <TextInput value={correo} onChangeText={setcorreo} className='rounded w-full bg-gray border-2' placeholder='example@gmail.com'></TextInput>
     </View>
     <View className=' m-4'>
         <Text>Password</Text>
-        <TextInput className='rounded w-full bg-gray border-2' placeholder='*********'></TextInput>
+        <TextInput value={pass} onChangeText={setpass} className='rounded w-full bg-gray border-2' placeholder='*********'></TextInput>
     </View>
     <View className='flex-row justify-center p-4'>
-        <Pressable className='bg-blue-500  rounded-full p-4'>
-            <Text className='color-white text-lg '>Enviar datos</Text>
+        <Pressable onPress={VeificarUse} className='bg-blue-500  rounded-full p-4'>
+            <Text  className='color-white text-lg '> {process.env.LOCAL} Enviar datos</Text>
         </Pressable>
     </View>
    </>
--- my-app/hooks/usuarios.js
+++ my-app/hooks/usuarios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, { useState } from 'react'
 
 export default function usuarios() {
-   const local='172.28.144.1'
+   const local='172.21.32.1'
    const [dataUser, setdataUser] = useState([])
    const FectUsuarios=async()=>{
     const {data}=await axios.get(`http://${local}:4000/usuarios`)
