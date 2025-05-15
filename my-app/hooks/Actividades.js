614b64780a41a34bfbac03441f29d212cb14a54b
joshua <condorenajoshua98@gmail.com>
2025-05-14 19:34:31 -0500

    Se agrego el hook actividades

--- my-app/hooks/Actividades.js
+++ my-app/hooks/Actividades.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, { useState } from 'react'
 
 export default function Actividades() {
-  const host='172.21.32.1'  
+  const host='172.19.96.1'  
   const [actividades, setactividades] = useState([])
   const FetchActividades=async()=>{
     const {data}=await axios.get(`http://${host}:4000/actividades`)
