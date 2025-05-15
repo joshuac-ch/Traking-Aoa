9041635b55b45be41451f248d5adde8363884bde
joshua <condorenajoshua98@gmail.com>
2025-05-14 19:34:13 -0500

    Se agregaron nuevos icons

--- my-app/assets/Icons.js
+++ my-app/assets/Icons.js
@@ -1,5 +1,8 @@
 import Fontisto from '@expo/vector-icons/Fontisto';
-
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 export const IconDate=()=>{
     return <Fontisto name="date" size={24} color="black" />
+}
+export const IconAdd=()=>{
+    return <FontAwesome6 name="add" size={24} color="black" />
 }
