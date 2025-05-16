25456959142611bfabce6eaec5e22123423d1157
joshua <condorenajoshua98@gmail.com>
2025-05-15 20:09:03 -0500

    Se permitio el uso de cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,4 +5,5 @@ app.use("/",router)
 const port=process.env.port|| 4000
 app.listen(port,()=>{
     console.log(`Entra al siguiente puerto: http://localhost:${port}`)
+    console.log(process.env.DB_NAME)
 })
