c654f34edc0ef07120449f9594e993f310bf7215
joshua <condorenajoshua98@gmail.com>
2025-05-16 19:41:08 -0500

    Se agregaron mas iconos

--- my-app/assets/Icons.js
+++ my-app/assets/Icons.js
@@ -1,8 +1,16 @@
 import Fontisto from '@expo/vector-icons/Fontisto';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
 export const IconDate=()=>{
     return <Fontisto name="date" size={24} color="black" />
 }
 export const IconAdd=()=>{
     return <FontAwesome6 name="add" size={24} color="black" />
+}
+export const IconLeft=()=>{
+    return <AntDesign name="rightcircle" size={24} color="black" />
+}
+export const IconDelete=()=>{
+    return <AntDesign name="delete" size={24} color="black" />
 }
