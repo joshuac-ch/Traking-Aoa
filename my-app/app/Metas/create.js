6c6860c84df52c206d4dfccd99d3c2c594367378
joshua <condorenajoshua98@gmail.com>
2025-05-22 19:13:11 -0500

    se modifico el diseño de metas/create

--- my-app/app/Metas/create.js
+++ my-app/app/Metas/create.js
@@ -1,10 +1,11 @@
 import { Picker } from '@react-native-picker/picker'
 import React,{useState } from 'react'
-import { Pressable, Text, TextInput, View } from 'react-native'
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import { DateTimePickerAndroid } from '@react-native-community/datetimepicker'
 import axios from 'axios'
 import Constants from "expo-constants"
 import { useUser } from '../../components/UserContext'
+import { Stack } from 'expo-router'
 export default function create() {
   const {user}=useUser()
   const [FormMetas, setFormMetas] = useState({
@@ -38,17 +39,19 @@ export default function create() {
   }
     return (
     <View>
-       <View>
+      <Stack.Screen options={{title:'Crear Metas'}}></Stack.Screen>
+       <View className='m-4'>
+        <View>
         <Text>Crear Nueva Meta</Text>
        </View>
        <View>
         <View>
             <Text>Titulo:</Text>
-            <TextInput onChangeText={text=>setFormMetas({...FormMetas,titulo:text})} value={FormMetas.titulo} placeholder='ingrese el nombre de su meta'></TextInput>
+            <TextInput style={styles.form_input} onChangeText={text=>setFormMetas({...FormMetas,titulo:text})} value={FormMetas.titulo} placeholder='ingrese el nombre de su meta'></TextInput>
         </View>
         <View>
             <Text>Descripcion: </Text>
-            <TextInput placeholder='ingrese mas detalles' value={FormMetas.descripcion} onChangeText={text=>setFormMetas({...FormMetas,descripcion:text})}></TextInput>
+            <TextInput style={styles.form_input} placeholder='ingrese mas detalles' value={FormMetas.descripcion} onChangeText={text=>setFormMetas({...FormMetas,descripcion:text})}></TextInput>
         </View>
         <View>
             <Text>Fecha Limite</Text>
@@ -56,12 +59,33 @@ export default function create() {
                 <Text>{FormMetas.fecha_limite.toLocaleDateString()}</Text>
             </Pressable>
         </View>
-        <View>
-          <Pressable onPress={EnviarDatos}>
-            <Text>Enviar Datos</Text>
+        <View  style={{alignItems:'center'}}>
+          <Pressable style={styles.btn_enmviar} onPress={EnviarDatos}>
+            <Text  style={{textAlign:'center'}}>Enviar Datos</Text>
           </Pressable>
         </View>
+       </View>       
        </View>
     </View>
   )
 }
+const styles=StyleSheet.create({
+  btn_enmviar:{
+    borderRadius:20,
+    width:200,
+    borderWidth:2,
+    padding:10,
+    marginTop:20,
+    borderStyle:'solid',
+    borderColor:'black'  
+
+  },
+  form_input:{
+    borderWidth:1,
+    borderStyle:'solid',
+    borderColor:'black',
+    padding:10,
+    width:'100%',
+    borderRadius:5
+  }
+})
