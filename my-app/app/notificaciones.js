153bbcb4d4c2c9886e2cd869858d1838b6259e33
joshua <condorenajoshua98@gmail.com>
2025-05-27 18:56:21 -0500

    Se agrego notificaciones

---
+++ my-app/app/notificaciones.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+export default function notificaciones() {
+  return (
+    <>
+    <View className='m-4'>
+        <View>
+          <Text>Bienvenido a notificaciones</Text>
+        </View>
+        <View>
+          
+        </View>
+    </View>
+    </>
+  )
+}
