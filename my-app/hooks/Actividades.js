bbfb3f160e8dbbd23ad7ce0233736703bf010009
joshua <condorenajoshua98@gmail.com>
2025-06-04 19:20:15 -0500

    Modificacion del hook actividades

--- my-app/hooks/Actividades.js
+++ my-app/hooks/Actividades.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import Constants from "expo-constants"
+import { useUser } from '../components/UserContext'
 export default function Actividades() {
+  const {user}=useUser()
   const host=Constants.expoConfig.extra.host
   const [actividades, setactividades] = useState([])
   const FetchActividades=async()=>{
-    const {data}=await axios.get(`http://${host}:4000/actividades`)
+    const {data}=await axios.get(`http://${host}:4000/actividades/${user.id}`)
     setactividades(data)
   }
     return {FetchActividades,actividades}
