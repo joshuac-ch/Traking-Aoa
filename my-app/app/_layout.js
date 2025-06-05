f921a4f7114db5bdd5a25c87788b21def6e2e355
joshua <condorenajoshua98@gmail.com>
2025-06-04 19:21:39 -0500

    Modificacion del layouy principal y el usercontext para mejor presentacion de los datos

--- my-app/app/_layout.js
+++ my-app/app/_layout.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import App from '../App'
-import { Text } from 'react-native'
-import { Slot, Stack } from 'expo-router'
+import { router, Slot, Stack } from 'expo-router'
 import { UserProvider } from '../components/UserContext'
 
 export default function _layout() {
   return (
    <>
    <UserProvider>
-    <Stack></Stack>
+      <Stack screenOptions={{title:'Home'}}>
+       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>{ /* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />SI eliminamos y ponemos header:false las rutas a actividades habitos y metas desaparecen? */}
    </UserProvider>
    </>
   )
--- my-app/components/UserContext.js
+++ my-app/components/UserContext.js
@@ -3,7 +3,8 @@ import { createContext, useContext, useState } from 'react'
 const UserContext = createContext()
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState({id:3})//Esto es solo prueba para que se inicialice
+  //const [user, setUser] = useState({id:2})//Esto es solo prueba para que se inicialice
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
