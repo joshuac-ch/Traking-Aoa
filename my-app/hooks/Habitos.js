bc0dfc7a258e85788ee54e139a7272656c902c9f
joshua <condorenajoshua98@gmail.com>
2025-06-05 19:40:49 -0500

    Hook habitos se limito la entrada

--- my-app/hooks/Habitos.js
+++ my-app/hooks/Habitos.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import Constants from "expo-constants"
+import { useUser } from '../components/UserContext';
 export default function Habitos() {
   const host=Constants.expoConfig.extra.host;
+  const {user}=useUser()
   const [habitos, sethabitos] = useState([])
   const FecthHabitos=async()=>{
     try{
-        const {data}=await axios.get(`http://${host}:4000/habitos`)
+        const {data}=await axios.get(`http://${host}:4000/habitos/${user.id}`)
         sethabitos(data)
     }catch(err){
         alert("Hubo un error",err.message)
