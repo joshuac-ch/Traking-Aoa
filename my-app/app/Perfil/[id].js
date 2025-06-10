e76ab07c8be950b8c85d2fc41aa908dcb991bd52
joshua <condorenajoshua98@gmail.com>
2025-06-09 19:41:10 -0500

    Modificaicon del perfil reutilizaciondel diseño

--- my-app/app/Perfil/[id].js
+++ my-app/app/Perfil/[id].js
@@ -119,11 +119,14 @@ const styles=StyleSheet.create({
     marginBottom:10
   },
   img_form:{
+    alignSelf:'center',
+    marginBottom:10,
+    
     borderWidth:2,
     borderColor:'black',
     borderStyle:'solid',
     padding:10,
-    borderRadius:10,
+    borderRadius:50,
     width:100,
     height:100,
   },
