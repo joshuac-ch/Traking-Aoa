008867c8de72c280fbf6dde031a497e04ba0f294
joshua <condorenajoshua98@gmail.com>
2025-06-14 20:21:17 -0500

    Mejora de actividades en los datos

--- my-app/app/Actividades/show/[show].js
+++ my-app/app/Actividades/show/[show].js
@@ -5,17 +5,41 @@ import { useLocalSearchParams } from 'expo-router'
 import { Image, StyleSheet, Text, View } from 'react-native'
 export default function show() {
     const host=constantes.expoConfig.extra.host
+    const [creatorUser, setcreatorUser] = useState("")
+    const [dataCreator, setdataCreator] = useState([])
     const {show}=useLocalSearchParams()
     const [dataActividaes, setdataActividaes] = useState([]) 
     const showActividades=async()=>{
         const {data}=await axios.get(`http://${host}:4000/actividades/s/${show}`)
         setdataActividaes(data)
+        setcreatorUser(data.usuario_id)
     }
+    
+    const ShowUserCreator=async()=>{
+        const {data}=await axios.get(`http://${host}:4000/usuarios/s/${creatorUser}`)
+        setdataCreator(data)
+    }
+
     useEffect(()=>{
         showActividades()
     },[])
+    useEffect(()=>{
+        if(showActividades){
+            ShowUserCreator()
+        }
+    },[showActividades])
     return (
     <>
+    <View style={{flexDirection:'row',padding:10}}>
+        <View>
+            <Image source={{uri:dataCreator.imagen}} style={{width:60,height:60,borderRadius:10}}></Image>
+        </View>
+        <View>
+            <Text style={{fontWeight:'bold'}}>Creador</Text>
+            <Text>{dataCreator.nombre}</Text>
+            <Text>{dataCreator.correo}</Text>
+        </View>
+    </View>
     <View>
         <Text>{dataActividaes.titulo}</Text>
     </View>
