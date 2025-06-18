7fcfcab51dcbbcff741cbf376f3cf9b1e2169843
joshua <condorenajoshua98@gmail.com>
2025-06-17 19:33:45 -0500

    Se mejoro el diseño de muestreo

--- my-app/app/Actividades/show/[show].js
+++ my-app/app/Actividades/show/[show].js
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from 'react'
 import constantes from "expo-constants"
 import { useLocalSearchParams } from 'expo-router'
 import { Image, StyleSheet, Text, View } from 'react-native'
-export default function show() {
+import { IconElipsis, IconHeart, IconReply } from '../../../assets/Icons'
+export default function ShowActividadUser() {
     const host=constantes.expoConfig.extra.host
     const [creatorUser, setcreatorUser] = useState("")
     const [dataCreator, setdataCreator] = useState([])
@@ -24,36 +25,86 @@ export default function show() {
         showActividades()
     },[])
     useEffect(()=>{
-        if(showActividades){
+        if(creatorUser){
             ShowUserCreator()
         }
-    },[showActividades])
+    },[creatorUser])
     return (
     <>
-    <View style={{flexDirection:'row',padding:10}}>
+    <View style={styles.contenedor_pub}>
+    
+    
+       <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center',margin:10}}>
+         <View style={{flexDirection:'row'}}>
         <View>
-            <Image source={{uri:dataCreator.imagen}} style={{width:60,height:60,borderRadius:10}}></Image>
+            {dataCreator.imagen&&(
+                <Image source={{uri:dataCreator.imagen}} style={{width:60,height:60,borderRadius:30,marginRight:10}}></Image>
+            )}
         </View>
         <View>
             <Text style={{fontWeight:'bold'}}>Creador</Text>
-            <Text>{dataCreator.nombre}</Text>
+            <View style={{flexDirection:'row'}}>
+                <Text style={{marginRight:10}}>{dataCreator.nombre}</Text>
+                <Text style={{color:'#75706f'}}>{dataActividaes.fecha?new Date(dataActividaes.fecha).toLocaleDateString():""}</Text>
+            </View>
             <Text>{dataCreator.correo}</Text>
         </View>
-    </View>
-    <View>
+        
+        </View>
+            <View >
+            <IconElipsis></IconElipsis>
+            </View>
+       </View>
+    <View  style={{padding:10}}>
         <Text>{dataActividaes.titulo}</Text>
+        <Text>{dataActividaes.descripcion}</Text>
     </View>
     <View>
-        <Image style={styles.imagen} source={{uri:dataActividaes.imagen}}></Image>
+        {dataActividaes.imagen&&(
+            <Image style={styles.imagen} source={{uri:dataActividaes.imagen}}></Image>
+        )}
+    </View>
+    <View style={styles.likes}>
+        <View style={styles.box}>
+            <IconHeart></IconHeart>
+            <Text style={{marginLeft:10}}>Love</Text>
+        </View>
+        <View style={styles.box}>
+            <IconReply></IconReply>
+            <Text style={{marginLeft:10}}>Compartir</Text>
+        </View>
+    </View>
     </View>
     </>
   )
 }
 const styles=StyleSheet.create({
+    box:{
+        flexDirection:'row',
+        alignItems:'center',
+        margin:10
+    },
+    likes:{
+        flexDirection:'row',
+        justifyContent:'space-around',
+        alignItems:'center'
+    },
+    contenedor_pub:{
+        margin:20,
+        
+        backgroundColor:'white',
+        borderWidth:2,
+        borderColor:'transparent',
+        borderStyle:'solid',
+        borderRadius:10
+    },
+    
     imagen:{
+        alignSelf:'center',
+        margin:10,
         width:150,
         height:200,
-        margin:10,
+        
         borderRadius:20
     }
 })
