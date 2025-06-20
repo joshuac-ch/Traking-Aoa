4644e28b055b4ff0e0faf9edafc782f952387e30
joshua <condorenajoshua98@gmail.com>
2025-06-19 19:29:05 -0500

    Se agrego variable global seguidor

--- my-app/components/HistorialProvider.js
+++ my-app/components/HistorialProvider.js
@@ -3,9 +3,10 @@ import React, { createContext, useContext, useState } from 'react'
 export default function HistorialProvider({children}) {
  
   const [historialC, sethistorialC] = useState([])
-  
+  const [seguidor, setseguidor] = useState(false)
+
     return (
-    <HistoriCreatecontext.Provider value={{historialC,sethistorialC}}>
+    <HistoriCreatecontext.Provider value={{historialC,sethistorialC,seguidor,setseguidor}}>
         {children}
     </HistoriCreatecontext.Provider>
     )
