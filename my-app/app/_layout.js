ff73003859c8e0ca9595fa41d5ed62324c51f1a7
joshua <condorenajoshua98@gmail.com>
2025-06-20 19:47:23 -0500

    Se modifico el layout

--- my-app/app/_layout.js
+++ my-app/app/_layout.js
@@ -2,14 +2,18 @@ import React from 'react'
 import App from '../App'
 import { router, Slot, Stack } from 'expo-router'
 import { UserProvider } from '../components/UserContext'
+import HistorialProvider from '../components/HistorialProvider'
 
 export default function _layout() {
   return (
    <>
    <UserProvider>
-      <Stack screenOptions={{title:'Home'}}>
+     <HistorialProvider>
+       <Stack screenOptions={{title:'Home'}}>
        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>{ /* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />SI eliminamos y ponemos header:false las rutas a actividades habitos y metas desaparecen? */}
+      </Stack>
+     </HistorialProvider>
+     { /* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />SI eliminamos y ponemos header:false las rutas a actividades habitos y metas desaparecen? */}
    </UserProvider>
    </>
   )
