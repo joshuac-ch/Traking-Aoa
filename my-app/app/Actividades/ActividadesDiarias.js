ead86a7b94c64754a9af725d9227d7054a82911b
joshua <condorenajoshua98@gmail.com>
2025-06-20 19:47:54 -0500

    modificacion del diseño del folder actividades

--- my-app/app/Actividades/ActividadesDiarias.js
+++ my-app/app/Actividades/ActividadesDiarias.js
@@ -1,16 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 import Actividades from '../../hooks/Actividades'
-import { IconAdd, IconDate, IconDelete } from '../../assets/Icons'
-import { Link, Stack } from 'expo-router'
+import { IconAdd, Iconclock, IconDate, IconDelete } from '../../assets/Icons'
+import { Link, Stack, useFocusEffect } from 'expo-router'
 import axios from 'axios'
 import constantes from "expo-constants"
+import { useUser } from '../../components/UserContext'
 export default function ActividadesDiarias() {
+  const {user}=useUser()  
   const {FetchActividades,actividades}=Actividades()
-  useEffect(()=>{
-    FetchActividades()
-  },[])
+  
   const EliminarActividad=async(id)=>{
+      
     try{
         const host=constantes.expoConfig.extra.host
         await axios.delete(`http://${host}:4000/actividades/d/${id}`)
@@ -19,14 +20,21 @@ export default function ActividadesDiarias() {
     } catch(err){
         alert("hubo un error",err.message)
     }
-  }  
+  }
+  useFocusEffect(
+    useCallback(()=>{
+        if(user.id){
+            FetchActividades()
+        }
+    },[user.id])
+  )  
   return (
     <>
     <Stack.Screen options={{title:"Actividades"}}></Stack.Screen>
     <ScrollView>
     <View className=''>
              <View style={styles.header_Acti}>
-            <Text className='font-black text-lg'>Actividades</Text>
+            <Text className='font-black text-lg'>Hoy</Text>
             <View >
                 <Link href={'/Actividades/Create'} asChild>
                 <Pressable >
@@ -37,28 +45,25 @@ export default function ActividadesDiarias() {
          </View>
     {actividades!=null?
     <View>
-        {actividades.map((a)=>{
+        {actividades.map((a,i)=>{
             return(
-                <Link key={a.id} href={`Actividades/${a.id}`} asChild>
+                <Link key={i} href={`Actividades/${a.id}`} asChild>
                     <Pressable>
                         <View style={styles.contenedor} key={a.id}>
-                        <View style={styles.subcontenedor} className=''>
-                            <Text>Titulo: {a.titulo}</Text>
-                            <Text>Descripcion: </Text>
-                            <Text className=''>{a.descripcion}</Text>
-                        </View>
-                        <View className=''>
-                             <View style={{alignItems:'flex-end'}}>
-                                <Pressable onPress={()=>EliminarActividad(a.id)}>
-                                    <IconDelete></IconDelete>
-                                </Pressable>
-                            </View>
-                            <View style={styles.cont_fecha}>
-                                <IconDate/>
-                                <Text> {a.fecha?new Date(a.fecha).toLocaleDateString():''}</Text>
-                            </View>
-                           
-                        </View>
+                            <View style={{flexDirection:'row',alignItems:'center'}}>
+                                <View style={{marginRight:10}}>
+                                    <Iconclock></Iconclock>
+                                </View>
+                                <View >
+                                    <Text>{a.titulo}</Text>                            
+                                    <Text> {a.fecha?new Date(a.fecha).toLocaleDateString():''}</Text>
+                                </View> 
+                            </View>                       
+                            <View style={{flexDirection:'row',alignItems:'center'}}>
+                                    <Pressable onPress={()=>EliminarActividad(a.id)}>
+                                        <IconDelete></IconDelete>
+                                    </Pressable>
+                            </View>                   
                         </View>
                     </Pressable>
                 </Link>
