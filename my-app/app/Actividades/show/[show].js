cc9095d9a9e4cd9279625bcea9f4d2f6db3f7af5
joshua <condorenajoshua98@gmail.com>
2025-06-23 19:39:14 -0500

    Se implemento meejoras en actividades publicacion show

--- my-app/app/Actividades/show/[show].js
+++ my-app/app/Actividades/show/[show].js
@@ -4,6 +4,7 @@ import constantes from "expo-constants"
 import { useLocalSearchParams } from 'expo-router'
 import { Image, StyleSheet, Text, View } from 'react-native'
 import { IconElipsis, IconHeart, IconReply } from '../../../assets/Icons'
+import PublicacionComponent from '../../../components/PublicacionComponent'
 export default function ShowActividadUser() {
     const host=constantes.expoConfig.extra.host
     const [creatorUser, setcreatorUser] = useState("")
@@ -31,80 +32,7 @@ export default function ShowActividadUser() {
     },[creatorUser])
     return (
     <>
-    <View style={styles.contenedor_pub}>
-    
-    
-       <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center',margin:10}}>
-         <View style={{flexDirection:'row'}}>
-        <View>
-            {dataCreator.imagen&&(
-                <Image source={{uri:dataCreator.imagen}} style={{width:60,height:60,borderRadius:30,marginRight:10}}></Image>
-            )}
-        </View>
-        <View>
-            <Text style={{fontWeight:'bold'}}>Creador</Text>
-            <View style={{flexDirection:'row'}}>
-                <Text style={{marginRight:10}}>{dataCreator.nombre}</Text>
-                <Text style={{color:'#75706f'}}>{dataActividaes.fecha?new Date(dataActividaes.fecha).toLocaleDateString():""}</Text>
-            </View>
-            <Text>{dataCreator.correo}</Text>
-        </View>
-        
-        </View>
-            <View >
-            <IconElipsis></IconElipsis>
-            </View>
-       </View>
-    <View  style={{padding:10}}>
-        <Text>{dataActividaes.titulo}</Text>
-        <Text>{dataActividaes.descripcion}</Text>
-    </View>
-    <View>
-        {dataActividaes.imagen&&(
-            <Image style={styles.imagen} source={{uri:dataActividaes.imagen}}></Image>
-        )}
-    </View>
-    <View style={styles.likes}>
-        <View style={styles.box}>
-            <IconHeart></IconHeart>
-            <Text style={{marginLeft:10}}>Love</Text>
-        </View>
-        <View style={styles.box}>
-            <IconReply></IconReply>
-            <Text style={{marginLeft:10}}>Compartir</Text>
-        </View>
-    </View>
-    </View>
+    <PublicacionComponent datosUser={dataCreator} datasRutina={dataActividaes}></PublicacionComponent>
     </>
   )
 }
-const styles=StyleSheet.create({
-    box:{
-        flexDirection:'row',
-        alignItems:'center',
-        margin:10
-    },
-    likes:{
-        flexDirection:'row',
-        justifyContent:'space-around',
-        alignItems:'center'
-    },
-    contenedor_pub:{
-        margin:20,
-        
-        backgroundColor:'white',
-        borderWidth:2,
-        borderColor:'transparent',
-        borderStyle:'solid',
-        borderRadius:10
-    },
-    
-    imagen:{
-        alignSelf:'center',
-        margin:10,
-        width:150,
-        height:200,
-        
-        borderRadius:20
-    }
-})
