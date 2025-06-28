fa96feda93f0696fced969d05f30d32c4997f14d
joshua <condorenajoshua98@gmail.com>
2025-06-27 19:24:02 -0500

    Se creo el controllador publicacionController

---
+++ backend/src/controllers/publicacionController.js
@@ -0,0 +1,39 @@
+const actividades_diarias = require("../../models/actividades_diarias")
+const publicaciones = require("../../models/publicaciones")
+const Seguidor = require("../../models/seguidores")
+const usuario = require("../../models/usuario")
+
+const getAllpublicaciones=async(req,res)=>{
+    const modelo=await publicaciones.findAll()
+    if(!modelo){
+        return res.status(404).json({message:"No se encontro el modelo"})
+    }
+    res.status(200).json(modelo)
+}
+const getPublicacionFollow=async(req,res)=>{
+    const {userID}=req.params
+    const modeloSeguidores=await Seguidor.findAll({
+        where:{seguidor_id:userID,estado:true}
+    })
+    const idSeguidos=modeloSeguidores.map((s)=>s.seguido_id)
+    const publicacion=await publicaciones.findAll({where:{usuario_id:idSeguidos}})
+    //desustrurta los datos
+    const publicacionExpandida=await Promise.all(
+        publicacion.map(async(p)=>{
+            let contendo=0
+            if(p.tipo=="Actividad"){
+                contendo=await actividades_diarias.findByPk(p.contenido_id)
+            }
+            let creador=await usuario.findByPk(p.usuario_id)
+            return{
+                id:p.id,
+                tipo:p.tipo,
+                creador,
+                contendo
+            }
+        })
+    )
+    res.status(200).json(publicacionExpandida) 
+}
+//Crear aqui el pos publicacion y enviarselo a actividaes y habitos 
+module.exports={getAllpublicaciones,getPublicacionFollow}
