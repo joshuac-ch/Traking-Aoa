965246f87d264139567e89c905ff9ca11d6b8195
joshua <condorenajoshua98@gmail.com>
2025-06-29 19:58:10 -0500

    Se hizo cambios en la plantilla pr

--- my-app/app/(tabs)/_layout.js
+++ my-app/app/(tabs)/_layout.js
@@ -1,15 +1,17 @@
 import { Tabs } from 'expo-router'
 import React from 'react'
-import { IconHome, IconSeach, IconUser } from '../../assets/Icons'
+import { IconBell, IconHome, IconSeach, IconUser } from '../../assets/Icons'
 
 export default function _layout() {
   return (
-    <Tabs>
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'purple',tabBarLabelStyle: { fontSize: 12 } }}>
         
-        <Tabs.Screen name='Panel' options={{title:'Home',tabBarIcon:()=><IconHome></IconHome>
-                                            ,headerShown:false}} ></Tabs.Screen>
-        <Tabs.Screen name='perfil' options={{title:'Perfil',tabBarIcon:()=><IconUser></IconUser>}}></Tabs.Screen>
-        <Tabs.Screen name='Buscardor' options={{title:'Buscador',tabBarIcon:()=><IconSeach></IconSeach>}}></Tabs.Screen>                                           
+        <Tabs.Screen name='Panel' options={{tabBarLabel:'Home',tabBarIcon:()=><IconHome></IconHome>}}></Tabs.Screen>
+         <Tabs.Screen name='Buscardor' options={{tabBarLabel:'Buscador',tabBarIcon:()=><IconSeach></IconSeach>}}></Tabs.Screen>
+         <Tabs.Screen name='notificaciones' options={{tabBarLabel:"Notificaciones",tabBarIcon:()=><IconBell></IconBell>}}></Tabs.Screen>     
+        <Tabs.Screen name='perfil' options={{tabBarLabel:"Perfil",tabBarIcon:()=><IconUser></IconUser>}}></Tabs.Screen>
+                                            
+        
     </Tabs>
   )
 }
