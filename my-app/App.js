92ef9ee444aa8bc31d8958a20376aaf742a95cbe
joshua <condorenajoshua98@gmail.com>
2025-07-01 19:22:15 -0500

    Componente principal

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import "./global.css"
-import AppPrincipal from './components/app';
+import AppPrincipal from './app/Login/app.js';
+
 export default function App() {
   return (
     <View style={styles.container}>
-    <AppPrincipal></AppPrincipal>
+   <AppPrincipal></AppPrincipal>
       <StatusBar style="auto" />
     </View>
   );
