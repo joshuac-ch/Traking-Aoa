0276f6083b2e5e7be6ec3475aee945842d6a606e
joshua <condorenajoshua98@gmail.com>
2025-07-07 19:30:52 -0500

    Reestructuracion de la migracion noti

--- backend/migrations/20250607233500-create-noti.js
+++ backend/migrations/20250607233500-create-noti.js
@@ -9,26 +9,29 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      icon: {
+      tipo: {
+        type:Sequelize.STRING}
+        ,
+      contenido_id:{
         type: Sequelize.STRING
+      },    
+      mensaje:{
+        type:Sequelize.STRING
       },
-      titulo: {
-        type: Sequelize.STRING
-      },
-      descripcion: {
-        type: Sequelize.STRING
-      },
-      hora: {
+      hora:{
         type: Sequelize.DATE
+      },    
+      emisor_id:{
+        type:Sequelize.BIGINT
       },
       usuario_id:{
-      type:Sequelize.BIGINT,
-           allowNull:false,
-           references:{
-             model:'usuario',
-             key:'id'
-           }
-    }
+           type:Sequelize.BIGINT,
+                 allowNull:false,
+                 references:{
+                   model:'usuario',
+                   key:'id'
+                 }
+          }
     });
   },
   async down(queryInterface, Sequelize) {
