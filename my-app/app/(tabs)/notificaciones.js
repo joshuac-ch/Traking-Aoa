dcc2486ed4318354f69f320e6f6022654804723d
joshua <condorenajoshua98@gmail.com>
2025-07-10 20:02:14 -0500

    UI se mejoro el diseño y nueva funcionalidad

--- my-app/app/(tabs)/notificaciones.js
+++ my-app/app/(tabs)/notificaciones.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { View,Text, StyleSheet, Image, ScrollView } from 'react-native'
 import { IconElipsis, IconHome, IconLeft, IconSeach, IconUser } from '../../assets/Icons'
 import { Stack, useFocusEffect } from 'expo-router'
@@ -38,6 +38,26 @@ export default function notificaciones() {
       }
     },[user.id])
   )
+  const [notificaciontotal, setnotificaciontotal] = useState({
+    notislikes:[],
+    notispost:[],
+    notifollow:[]
+  })
+  const ObtenerAllNotificaciones=async()=>{
+    setnotificaciontotal({
+      notislikes:likesuser,
+      notispost:notiUser,
+      notisfollow:notifollow      
+    })
+    console.log(JSON.stringify(notificaciontotal,null,2))
+  } 
+   useFocusEffect(
+    useCallback(()=>{
+      if(likesuser.length>0 && notiUser.length>0 && notifollow.length>0){
+       ObtenerAllNotificaciones()
+      }   
+    },[likesuser,notiUser,notifollow])
+  )
   
   //Se creo por ahora que se vena las notificaciones si seguimos al usuarios
   //crear en el post (listo)
@@ -48,16 +68,24 @@ export default function notificaciones() {
     <ScrollView>
     <Stack.Screen options={{headerShown:false}}></Stack.Screen>
     <View style={{marginTop:55}}></View>
-    <View className='m-4'>
-        <View>
-          <Text>Bandeja de Entrada</Text>
-        </View>
-        {notiUser.length>0?
-        <View>
-          {notiUser.map((n,i)=>{
+    <View >
+     
+    <View>
+          <View>
+            <Text>Bandeja de entrada</Text>
+          </View>
+          <View>
+            {likesuser.length>0 || 
+             notiUser.length>0 || 
+             notifollow.length>0?
+            (
+              <View style={styles.principal}>
+            {
+            notificaciontotal?.notispost?.length>0?
+            notificaciontotal.notispost.map((n,i)=>{
             return(
               n.noti.tipo=="Post_Actividad" || n.noti.tipo=="Post_habito"?
-              <View key={i} style={{flexDirection:'row',justifyContent:"space-between",alignItems:"center"}} key={i}>
+              <View key={i} style={styles.contenedorpost}>
                 <View style={{flexDirection:"row"}}>
                   <View>                  
                     <Image source={{uri:n.creador.imagen}} style={{height:50,width:50,borderRadius:50,marginRight:10}}></Image>
@@ -74,20 +102,15 @@ export default function notificaciones() {
                 </View>
                 
               </View>
-              :
-              ""              
+              :null                           
             )
-          })}          
-        </View>
-        :
-        <View>
-          <Text>No hay notificaciones actualmente</Text>
-        </View>
-        }
-        {likesuser.length>0?          
-            likesuser.map((n,i)=>{
+          }) 
+            :null
+          } 
+          {notificaciontotal?.notislikes?.length>0?          
+            notificaciontotal.notislikes.map((n,i)=>{
               return(               
-                <View key={i} style={{flexDirection:"row",justifyContent:'space-between'}}>
+                <View key={i} style={styles.contendorlikes}>
                    <View style={{flexDirection:"row"}}>
                   <View>                  
                     <Image source={{uri:n.creador.imagen}} style={{height:50,width:50,borderRadius:50,marginRight:10}}></Image>
@@ -103,14 +126,13 @@ export default function notificaciones() {
                 
               )
             })        
-        :""
-        
+        :null        
         }
-        {
-          notifollow.length>0?
-          notifollow.map((n,i)=>{
+              
+        { notificaciontotal?.notisfollow?.length>0?
+          notificaciontotal?.notisfollow.map((n,i)=>{
             return(
-              <View key={i} style={{flexDirection:"row",justifyContent:'space-between'}}>
+              <View key={i} style={styles.contendorfollow}>
                   <View style={{flexDirection:"row"}}>
                   <View>                  
                     <Image source={{uri:n.user.imagen}} style={{height:50,width:50,borderRadius:50,marginRight:10}}></Image>
@@ -124,8 +146,14 @@ export default function notificaciones() {
               </View>
             )
           })
-          :""
-        }        
+          :null
+        }
+            </View>
+            )
+        :<Text>No hay notificaciones actualmente</Text>}
+
+        </View>
+      </View>        
         
     </View>
     </ScrollView>
@@ -133,6 +161,42 @@ export default function notificaciones() {
   )
 }
 const styles=StyleSheet.create({
+  principal:{
+    borderColor:"black",
+    borderStyle:"solid",
+      
+    paddingTop:10,
+    paddingBottom:10,
+    borderBottomWidth:2,
+  },
+  contenedorpost:{
+    paddingTop:10,
+    paddingBottom:10,
+    flexDirection:'row',    
+    borderColor:"black",
+    borderStyle:"solid",
+    borderTopWidth:2,
+    justifyContent:"space-between",
+    alignItems:"center"
+  },
+  contendorlikes:{
+    flexDirection:"row",
+    borderColor:"black",
+    borderStyle:"solid",
+    borderTopWidth:2,
+    paddingTop:10,
+    paddingBottom:10,
+    justifyContent:'space-between'
+  },
+  contendorfollow:{
+    flexDirection:"row",
+    borderColor:"black",
+    borderStyle:"solid",
+    borderTopWidth:2,    
+    paddingTop:10,
+    paddingBottom:10,
+    justifyContent:'space-between'
+  },
   acti:{
     marginTop:10,
     marginBottom:10,
