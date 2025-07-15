8b2e61e233b010d2ac350abba2bb76076484ec15
joshua <condorenajoshua98@gmail.com>
2025-07-14 19:33:07 -0500

    Se agrego la migracion de comentarios

---
+++ backend/migrations/20250714231643-create-comentario.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('comentarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      publicacion_id:{
+            type:Sequelize.BIGINT,
+            references:{
+            model:"publicaciones",
+            key:"id"
+      }
+      },
+      usuario_id:{
+            type:Sequelize.BIGINT,
+            references:{
+              model:"usuario",
+              key:"id"
+            }
+      },
+      like_user:{
+            type:Sequelize.STRING
+      },
+      tipo:{
+            type:Sequelize.STRING
+      },
+      comentario:{
+            type: Sequelize.TEXT
+      }
+        
+      
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('comentarios');
+  }
+};
