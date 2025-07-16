526f88f2554663583c67412ef7781c9e77441828
joshua <condorenajoshua98@gmail.com>
2025-07-15 20:37:04 -0500

    Nueva funcion de comentario agregada

--- backend/src/controllers/comentarioController.js
+++ backend/src/controllers/comentarioController.js
@@ -1,4 +1,5 @@
 const Comentario=require("../../models/comentario")
+const usuario = require("../../models/usuario")
 const CreateComentario=async(req,res)=>{
     try{
         const {publicacion_id,usuario_id,like_user,tipo,comentario}=req.body
@@ -15,4 +16,25 @@ const CreateComentario=async(req,res)=>{
         throw new Error(`Hubo un error ${err.message}`);        
     }
 }
-module.exports={CreateComentario}
+const GetComentarioPublicacion=async(req,res)=>{
+    try{
+        const {pubID}=req.params
+        const modelo=await Comentario.findAll({where:{publicacion_id:pubID}})
+        if(!modelo){
+            return res.status(404).json({message:"No se encontro ese comentario"})
+        }
+        const modeloExpandido=await Promise.all(
+            modelo.map(async(m)=>{
+                let creador=await usuario.findByPk(m.usuario_id)
+            return{
+                comentario:m,
+                creador
+            }
+            })            
+        )
+        res.status(200).json(modeloExpandido)
+    }catch(err){
+        console.err(err.message)
+    }
+}
+module.exports={CreateComentario,GetComentarioPublicacion}
