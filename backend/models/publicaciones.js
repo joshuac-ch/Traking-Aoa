6b961c0924fc7bc4fed08e39d2405c84b46239b7
joshua <condorenajoshua98@gmail.com>
2025-07-16 19:37:08 -0500

    Se modifico el modelo publicacion

--- backend/models/publicaciones.js
+++ backend/models/publicaciones.js
@@ -12,6 +12,7 @@ const sequelize=require("../src/config/database")
     static associate(models) {
       publicaciones.belongsTo(models.usuario,{foreignKey:"usuario_id"})
       publicaciones.hasMany(models.likes_publicacion,{foreignKey:"publicacion_id"})
+      publicaciones.hasMany(models.comentario,{foreignKey:"publicacion_id"})
       // define association here
     }
   }
