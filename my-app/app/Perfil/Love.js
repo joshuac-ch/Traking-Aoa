d2c342795600aa21a04a0243d7d687460bb97a60
joshua <condorenajoshua98@gmail.com>
2025-07-18 19:41:24 -0500

    Se agrego nuevo parametro

--- my-app/app/Perfil/Love.js
+++ my-app/app/Perfil/Love.js
@@ -2,17 +2,16 @@ import axios from 'axios'
 import React, { useCallback, useState } from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native'
 import { useUser } from '../../components/UserContext'
-import { Link, useFocusEffect } from 'expo-router'
+import { Link, router, useFocusEffect } from 'expo-router'
 import constantes from "expo-constants"
 import { Pressable } from 'react-native'
 export default function Love() {
     const [dataLove, setdataLove] = useState([])
     const host=constantes.expoConfig.extra.host
-    const {user}=useUser() 
+    const {user}=useUser()     
     const ShowLovePost=async()=>{
         const {data}=await axios.get(`http://${host}:4000/seguidor/loves/${user.id}`)
-        setdataLove(data)
-       
+        setdataLove(data)       
     }
     useFocusEffect(
         useCallback(()=>{
@@ -21,6 +20,11 @@ export default function Love() {
             }
         },[user.id])
     )
+   //crear un boton para perfil editar qeu esta sobreponido y aho lo rediriha a user edit
+   //al entrar al perfil que el miso usuario que creo sus publicaciones puede verlas hasta los comentarios   
+   //en buscar implementar el params  
+   //crear la nmotificacion comentarios sin seguidor
+    
   return (
     <>
     <View>
@@ -29,12 +33,14 @@ export default function Love() {
         <View style={{flexDirection:'row',justifyContent:'center',flexWrap:'wrap'}}>
         {dataLove.map((p)=>{
             return(                
-                    <Link key={p.id} asChild href={`/${p.tipo}/show/${p.valor.id}`}>
+                    <Link key={p.id} asChild href={{
+                        pathname:`/${p.tipo}/show/${p.valor.id}`,
+                        params:{publi:p.publicacion_id}}}>
                     <Pressable key={p.id}>                   
                     <View >
                         <View style={styles.contenedor_imagen}>
                         <Image style={{width:129.4,height:180,borderTopLeftRadius:8,borderTopRightRadius:8,alignSelf:'center'}} source={{uri:p.valor.imagen}}></Image>
-                        <Text style={{textAlign:'left',padding:5}}>{p.valor.titulo}</Text>
+                        <Text style={{textAlign:'left',padding:5}}>{p.valor.titulo.length>13?p.valor.titulo.slice(0,13)+"...":p.valor.titulo}</Text>
                         </View> 
                     </View>
                     </Pressable>                                       
