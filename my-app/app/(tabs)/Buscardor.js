49b91337e851435541a71dfaac4ae4fe4f57683e
joshua <condorenajoshua98@gmail.com>
2025-07-19 19:56:51 -0500

    Se hizo ajustes al scrip buscador

--- my-app/app/(tabs)/Buscardor.js
+++ my-app/app/(tabs)/Buscardor.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { StyleSheet, TextInput,Text, View, ScrollView, Pressable, Image, Button } from 'react-native'
 import { IconActivity, IconSeach, IconUser } from '../../assets/Icons'
 import Metas from '../../hooks/Metas'
 import Habitos from '../../hooks/Habitos'
 import Actividades from '../../hooks/Actividades'
-import { Link, router, Stack, useLocalSearchParams, useRouter } from 'expo-router'
+import { Link, router, Stack, useFocusEffect, useLocalSearchParams, useRouter } from 'expo-router'
 import usuarios from '../../hooks/usuarios'
 import { useUser } from '../../components/UserContext'
 import BusSearch from '../Buscador/bus'
 import { useHistoryial } from '../../components/HistorialProvider'
-
+import axios from 'axios'
+import constantes from "expo-constants"
 export default function Buscardor() {
     const {FectUsuarios,dataUser}=usuarios()
     const {FectMetas,metas}=Metas()
@@ -21,79 +22,41 @@ export default function Buscardor() {
     const {user}=useUser()
     const navegar=useRouter()
     const {historialC}=useHistoryial()
-    //const entradaobjeto=historialC?historialC:"no hay datos"
-    //console.log(entradaobjeto)
-        
-    {/*
-      useEffect(()=>{
-        FectMetas(),
-        FecthHabitos(),
-        FetchActividades(),
-        FectUsuarios()
-    },[])
-    useEffect(()=>{
-        if(datosbuscados.trim()!=""){
-            
-            const filtrado=metas.filter((m)=>
-                m.titulo.toLowerCase().includes(datosbuscados.toLowerCase()))
-            const filterActividad=actividades.filter((a)=>
-                a.titulo.toLowerCase().includes(datosbuscados.toLowerCase()))
-            const filterhabitos=habitos.filter((h)=>
-                h.titulo.toLowerCase().includes(datosbuscados.toLowerCase()))
-            const filtarUsers=dataUser.filter((u)=>
-                u.nombre!=user.nombre && u.nombre.includes(datosbuscados.toLowerCase())
-            )
-            const resultadosActuales=[
-              ...filtarUsers.map(a=>({...a,tipo:"Usuario"})),  
-              ...filtrado.map(item=>({...item,tipo:'Metas'})),
-              ...filterActividad.map(item=>({...item,tipo:'Actividades'})),
-              ...filterhabitos.map(item=>({...item,tipo:'Habitos'}))
-            
-            ]
-            setresultado(resultadosActuales)
-
-            //if(palanca){
-            //    sethistorial(resultadosActuales)
-            //    setpalanca(false)
-            //}
-        }else{
-            setresultado('')
-        }       
-    },[datosbuscados,metas,actividades,habitos])
-    const guardarbusqueda=(id,titulo,tipo,descripcion)=>{
-        //setpalanca(true)
-        
-        sethistorial(prev=>{
-            const existe=prev.some(item=>
-                item.tipo===tipo&&
-                item.titulo===titulo &&
-                item.descripcion===descripcion
-            )
-            if(existe){
-                return prev
-            }
-            return [...prev,{id,tipo,titulo,descripcion}]            
-    })        
-    }  
-        */}
+    
     const EnviarSearch=async()=>{
         navegar.push({
             pathname:"/Buscador/bus",
             params:{entrada:datosbuscados}
         })
     }
+    const [creador, setcreador] = useState([])
+    const host=constantes.expoConfig.extra.host
+    const MostCreador=async()=>{
+        const {data}=await axios.get(`http://${host}:4000/usuarios/s/${user.id}`)
+        setcreador(data)
+    }
+    useFocusEffect(
+        useCallback(()=>{
+            MostCreador()
+        },[])
+    )
+
     return (
    <>
    <ScrollView>
     
      <Stack.Screen options={{headerShown:false}}></Stack.Screen>
-    <View style={styles.buscar}>
-    <TextInput onChangeText={text=>setdatosbuscados(text)} value={datosbuscados}  placeholder='buscar...'></TextInput>
-   
-    <Pressable onPress={(EnviarSearch)}>
-        <IconSeach style={styles.icon_Search}></IconSeach>
-    </Pressable>
-   </View>
+     <View style={{flexDirection:"row",alignItems:"center",justifyContent:"center",marginTop:60}}>
+        <View>
+            <Image source={{uri:creador.imagen}} style={{width:50,borderRadius:50,height:50}}></Image>
+        </View>
+        <View style={styles.buscar}>
+            <TextInput onChangeText={text=>setdatosbuscados(text)} value={datosbuscados}  placeholder='buscar...'></TextInput>
+            <Pressable onPress={(EnviarSearch)}>
+                <IconSeach style={styles.icon_Search}></IconSeach>
+            </Pressable>
+        </View>
+     </View>
    <View style={{flexDirection:'column',justifyContent:'space-around',margin:20}}>
         {historialC.map((h,i)=>{           
             return(
@@ -147,120 +110,8 @@ export default function Buscardor() {
                    </Link>
                 )
         })}
-   </View>
-   {
-   /*
-   <View style={{flexDirection:'row',justifyContent:'space-around',margin:20}}>
-    <View>
-        <Text>Actividades</Text>
-    </View>
-    <View>
-        <Text>Usuarios</Text>
-    </View>
+   </View> 
    
-   </View>
-   <View>
-    <View>
-        
-    </View>
-    <View>
-        {resultado.length>0?
-        resultado.map((m,i)=>{
-            return(
-                m.tipo=="Usuario"?
-                <Link key={i} href={`/Perfil/users/${m.id}`} asChild>
-                    <Pressable onPress={()=>guardarbusqueda(m.id,m.nombre,m.tipo,m.correo)}>
-                        <View style={styles.contendor_user}>
-                            <Image source={{uri:m.imagen}} style={{width:50,height:50,borderRadius:50,margin:10}}></Image>
-                            <View>
-                                <Text>{m.nombre}</Text>
-                                <Text style={{fontWeight:'bold'}}>{m.correo}</Text>
-                            </View>
-                        </View>
-                    </Pressable>
-                </Link>
-                :
-                <Link  key={i} href={`/${m.tipo}/${m.id}`} asChild>
-                    <Pressable onPress={()=>guardarbusqueda(m.id,m.titulo,m.tipo,m.descripcion)}>
-                        <View style={styles.contenedor_mostrar}>
-                            <View style={{marginLeft:10,marginRight:10}}>
-                                <IconActivity></IconActivity>
-                            </View>
-                            <View>
-                                 <Text>{m.tipo}</Text>
-                                 <Text>{m.titulo}</Text>
-                            </View>                            
-                        </View>
-                    </Pressable>
-                </Link>
-            )
-        })
-        :historial.length>0?
-            historial.map((h,i)=>{
-                
-                //el buscardor de ahora sera global y buscarda los show ya no los nuestros 
-                // si quieres buscar los tuyos podras buscarlos en cada categoria correspondiente
-                //para mas adelante
-                //la funcion compartir estara relaciondad con los show que se crearon para compartir y tambien compartir el perfil hal hacer click
-                               
-                return(
-                    h.tipo=="Usuario"?
-                    <Link key={i} href={`/Perfil/users/${h.id}`} asChild>
-                    <Pressable>
-                        <View style={styles.contendor_buscador} key={i}>
-                            <View style={styles.box}>
-                            <View style={{flexDirection:'row',alignItems:'center'}}>
-                                <View style={{marginRight:10}}>
-                                   <IconUser></IconUser> 
-                                </View>                           
-                                <View>
-                                    <Text>{h.titulo}</Text>
-                                <Text>{h.descripcion}</Text>
-                                </View>
-                            </View>
-                            <View style={styles.box_tipe} >                                
-                                 <Text style={{textAlign:'center',color:'white'}}>{h.tipo}</Text>
-                            </View>                           
-                            </View>
-                            
-                            
-                        </View>
-                    </Pressable>
-                    </Link>
-                    :        
-                    <Link key={i} href={`/${h.tipo}/${h.id}`} asChild>
-                    <Pressable>
-                        <View style={styles.contendor_buscador} key={i}>
-                            <View style={styles.box}>
-                             <View style={{flexDirection:'row',alignContent:'center'}}>
-                                <View style={{marginRight:10}}>
-                                <IconActivity></IconActivity>
-                             </View>
-                            <View  >
-                                <Text>Titulo: </Text>
-                                <Text>{h.titulo}</Text>
-                                <Text>{h.descripcion}</Text>
-                            </View>
-                             </View>
-                            <View style={styles.box_tipe} >                                
-                                 <Text style={{textAlign:'center',color:'white'}}>{h.tipo}</Text>
-                            </View>
-                           
-                            </View>
-                            
-                            
-                        </View>
-                    </Pressable>
-                   </Link>
-                )
-            })
-        :
-        <View style={styles.contenedor_no_datos}>
-            <Text style={styles.no_datos}>no ha datos</Text>
-        </View>}
-    </View>
-   </View>
-   */}
    </ScrollView>
    </>
   )
@@ -301,19 +152,16 @@ const styles=StyleSheet.create({
         justifyContent:'space-between',
         alignItems:'center',
         alignSelf:'center',
-
-        marginLeft:10,
-        marginRight:10,
-        marginBottom:10,
-        marginTop:55,
-
-        padding:3,
-                
+        
+        marginLeft:5,
+        marginRight:5,
+        
+        padding:3,  
         borderStyle:'solid',
         borderWidth:2,
-        width:350,
+        width:300,
         borderColor:'black',
-        borderRadius:10,        
+        borderRadius:50,        
     }
    
     ,
