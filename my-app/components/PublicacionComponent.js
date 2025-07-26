e2b0d83bc1b69ad54cb6414c8530e95cea14a9db
joshua <condorenajoshua98@gmail.com>
2025-07-25 19:45:10 -0500

    Se implemento la imagen expandida

--- my-app/components/PublicacionComponent.js
+++ my-app/components/PublicacionComponent.js
@@ -1,54 +1,67 @@
-import React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Image, Modal, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 import { IconElipsis, IconHeart, IconReply } from '../assets/Icons'
 import Comentario from './Comentario'
 
 export default function PublicacionComponent({datosUser,datasRutina,publicacionID}) {
+  const [imagenExpandida, setimagenExpandida] = useState(false)    
   return (
-    <View style={styles.contenedor_principal}>
-        <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center',margin:10}}>
-            <View style={{display:'flex',flexDirection:'row'}}>
-        <View >
-            <Image source={{uri:datosUser.imagen}} style={{width:60,height:60,borderRadius:10,marginRight:10}}></Image>
-        </View>
-        <View>
-            <Text style={{fontWeight:'bold'}}>Creador</Text>
-            <View style={{flexDirection:'row'}}>
-                <Text style={{marginRight:20}}>{datosUser.nombre}</Text>
-                <Text style={{color:'#75706f'}}>{datasRutina.fecha_inicio?new Date(datasRutina.fecha_inicio).toLocaleDateString():""}</Text>
+    <ScrollView>
+        <View style={styles.contenedor_principal}>
+            <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center',margin:10}}>
+                <View style={{display:'flex',flexDirection:'row'}}>
+                    <View >
+                        <Image source={{uri:datosUser.imagen}} style={{width:60,height:60,borderRadius:10,marginRight:10}}></Image>
+                    </View>
+                    <View>
+                        <Text style={{fontWeight:'bold'}}>Creador</Text>
+                        <View style={{flexDirection:'row'}}>
+                            <Text style={{marginRight:20}}>{datosUser.nombre}</Text>
+                            <Text style={{color:'#75706f'}}>{datasRutina.fecha_inicio?new Date(datasRutina.fecha_inicio).toLocaleDateString():""}</Text>
+                        </View>
+                        <Text>{datosUser.correo}</Text>
+                    </View>        
+                </View>
+                <View>
+                    <IconElipsis></IconElipsis>
+                </View>
             </View>
-            <Text>{datosUser.correo}</Text>
-        </View>        
-       </View>
-       <View>
-        <IconElipsis></IconElipsis>
-       </View>
-        </View>
     
-       <View >
-         <View style={{padding:10}}>
-            <Text>{datasRutina.titulo}</Text>
-            <Text>{datasRutina.descripcion}</Text>
-        </View>        
-        <View>
-            <Image style={styles.imagen} source={{uri:datasRutina.imagen}}></Image>
-        </View>
-        <View style={{flexDirection:"row",justifyContent:'space-around',alignItems:'center'}}>
-            <View  style={{flexDirection:'row',margin:10,alignItems:'center'}}>
-                <IconHeart></IconHeart>
-                <Text style={{marginLeft:10}}>Love</Text>
+            <View >
+                <View style={{padding:10}}>
+                    <Text>{datasRutina.titulo}</Text>
+                    <Text>{datasRutina.descripcion}</Text>
+                </View>        
+                <View>
+                    <Pressable onPress={()=>setimagenExpandida(true)}>
+                         <Image style={styles.imagen} source={{ uri: datasRutina.imagen }} />
+                    </Pressable>
+                    <Modal visible={imagenExpandida} animationType="fade">
+                        <View style={styles.modal}>
+                        <Pressable style={styles.zona} onPress={()=>setimagenExpandida(false)}>
+                             <Image style={styles.imagen_expandida} source={{ uri: datasRutina.imagen }} />
+                        </Pressable>
+                        </View>                    
+                    </Modal>             
+                
+               
+                </View>
+                <View style={{flexDirection:"row",justifyContent:'space-around',alignItems:'center'}}>
+                    <View  style={{flexDirection:'row',margin:10,alignItems:'center'}}>
+                        <IconHeart></IconHeart>
+                        <Text style={{marginLeft:10}}>Love</Text>
+                    </View>
+                    <View style={{flexDirection:'row',margin:10,alignItems:'center'}}>
+                        <IconReply></IconReply>
+                        <Text style={{marginLeft:10}}>Compartir</Text>
+                    </View>
+                </View>
+                <View>
+                    <Comentario pubID={publicacionID}></Comentario>
+                </View>
             </View>
-            <View style={{flexDirection:'row',margin:10,alignItems:'center'}}>
-                <IconReply></IconReply>
-                <Text style={{marginLeft:10}}>Compartir</Text>
-            </View>
-
         </View>
-       <View>
-        <Comentario pubID={publicacionID}></Comentario>
-       </View>
-       </View>
-    </View>
+    </ScrollView>
   )
 }
 const styles=StyleSheet.create({
@@ -61,11 +74,23 @@ const styles=StyleSheet.create({
         borderStyle:'solid',
         borderColor:"transparent"
     },
+    zona:{
+       flex:1,
+    },
+    modal:{
+        flex:1,
+        backgroundColor:"black",
+    },
     imagen:{
         margin:10,
-        width:150,
-        height:200,
+        width:250,
+        height:300,
         alignSelf:'center',
         borderRadius:20,        
-    }
+    },
+    imagen_expandida:{
+        width:"100%",
+        height:"100%",
+        resizeMode:"contain"
+    },
 })
