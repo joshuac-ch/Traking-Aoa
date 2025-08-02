e3a14c438fe7a84a07a65cfa2fb8f0d66099611e
joshua <condorenajoshua98@gmail.com>
2025-08-01 20:05:57 -0500

    Se mejoro la interfaz del login

--- my-app/app/Login/app.js
+++ my-app/app/Login/app.js
@@ -1,11 +1,14 @@
 import { useCallback, useEffect, useState } from 'react'
 
-import {Image, Pressable, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native'
+import {Image, ImageBackground, Pressable, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native'
 import usuarios from '../../hooks/usuarios'
 import { Link, Stack, useFocusEffect, useRouter } from 'expo-router'
 import { useUser } from '../../components/UserContext'
 import portada from "../../assets/portada1.png"
 import imagen_principal from "../../assets/imagen1.png"
+import login from "../../assets/login1.png"
+import ramas from "../../assets/ramam.png"
+import fondo from "../../assets/fondo_flora.png"
 export default function AppPrincipal() {
     const {setUser}=useUser()
   const {FectUsuarios,dataUser}=usuarios()
@@ -52,28 +55,63 @@ export default function AppPrincipal() {
         </View>
        )
     }
-    
+    const LoginLogo=()=>{
+        return(
+            <Image source={login} style={{width:120,height:120,borderRadius:20}}></Image>
+        )
+    }
+    const ConjuntoFlores=({arriba,abajo=0,lef})=>{
+        return(
+            <View style={{position:"absolute",bottom:abajo,top:arriba,left:lef}}>
+                <Image source={imagen_principal} style={{width:100,height:100,borderRadius:20}}></Image>
+            </View>
+        )
+    }
+    const Ramas=()=>{
+        return(
+            <Image source={ramas} style={{width:250,height:200}}></Image>
+        )
+    } 
   return (
    <>
-  <View style={{position:"absolute",top:30,left:-20}}>
+   
+   
+   <Stack.Screen options={{headerShown:false}}></Stack.Screen>
+   <View style={{flex: 1,backgroundColor:"#807c76"}}>
+    <ImageBackground source={fondo} style={styles.fondo} resizeMode='stretch' imageStyle={{alignSelf:"flex-end"}} >
+    {/*
+    <View style={{position:"absolute",top:30,left:-20}}>
+   
      <Nube top={50} size={80} left={150}></Nube>
      <Nube top={20} size={110} left={100}></Nube>
      <Nube top={50} size={80} left={50}></Nube>
-  </View>
+    </View>
   <View style={{position:"absolute",bottom:250,left:160}}>
      <Nube top={50} size={30} ancho={40} left={120}></Nube>
      <Nube top={30} size={50} ancho={40} left={100}></Nube>
      <Nube top={50} size={30} ancho={40} left={80}></Nube>
   </View>
+  <View style={{position:"absolute",top:300,left:10}}>
+    <Image source={ramas} style={{width:`${60}`,height:`${60}`}}></Image>    
+  </View>
  <LadyFlor abajo={0} lef={10}></LadyFlor>
  <LadyFlor abajo={0} isq={0} arriba={0}></LadyFlor>
-  <View style={{position:"absolute",top:170,
-        left:155}}>
-    <Image source={imagen_principal} style={{width:100,height:100,borderRadius:20}}></Image>
-  </View>
-    <Stack.Screen options={{headerShown:false}}></Stack.Screen>
+    <ConjuntoFlores></ConjuntoFlores>
+    <View style={{position:"absolute",top:0,left:170}}>
+    <Ramas></Ramas>
+   </View>
+   <View style={{position:"absolute",bottom:0}}>
+    <Ramas></Ramas>
+   </View>
+   <View style={{position:"absolute",bottom:0,left:200}}>
+    <Ramas></Ramas>
+   </View>
+    */}
+    
     <View style={styles.contenedor}>
-        <Text className="text-4xl text-center font-black">Login</Text>
+    <View style={{alignItems:"center"}}>
+        <LoginLogo></LoginLogo>    
+    </View> 
     <View className='m-4'>
         <Text >Correo</Text>
         <TextInput value={correo} onChangeText={setcorreo} className='rounded w-full bg-gray border-2' placeholder='example@gmail.com'></TextInput>
@@ -91,40 +129,33 @@ export default function AppPrincipal() {
         </Pressable>
     </View>
     </View>
+    </ImageBackground>
+  
+   </View>
+  
    </>
 )}
+//
 const styles=StyleSheet.create({
+    fondo: {
+       
+    position: 'absolute',
+      bottom: -0,
+      width: '100%',
+      height: '100%',
+    
+    flex: 1,    
+    alignItems: 'center',
+  },
+    text_input:{
+        borderRadius:0,
+    },
     contenedor:{
+        width:"80%",
         flex:1,
+        
         justifyContent:'center',
         alignContent:'center'
-    },
-    estilos_nubes1:{
-        position:"absolute",
-        top:50,
-        left:150,
-        height:80,
-        width:80,
-        backgroundColor:"pink",
-        borderRadius:99        
-    },
-    estilos_nubes2:{
-        position:"absolute",
-        top:20,
-        left:100,
-        height:110,
-        width:80,
-        backgroundColor:"pink",
-        borderRadius:99        
-    },
-    estilos_nubes3:{
-        position:"absolute",
-        top:50,
-        left:50,
-        height:80,
-        width:80,
-        backgroundColor:"pink",
-        borderRadius:99        
-    }
+    },   
     
 })
