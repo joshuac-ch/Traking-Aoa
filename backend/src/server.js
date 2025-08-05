6f570969e45eec8fc137183104c155596a89c357
joshua <condorenajoshua98@gmail.com>
2025-08-04 20:39:00 -0500

    SE modifico la entrada principal

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,8 @@ const app=require('./app')
 const router=require('./routes/useroutes')
 app.use("/",router)
 const port=process.env.port|| 4000
+ruta_app="192.168.18.24"
 app.listen(port,()=>{
-    console.log(`Entra al siguiente puerto: http://localhost:${port}`)
+    console.log(`Entra al siguiente puerto: http://${ruta_app}:${port}`)
     console.log(process.env.DB_NAME)
 })
