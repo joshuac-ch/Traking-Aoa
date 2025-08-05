b770627506cb4774b51e68df6f7110c2e50e2dc8
joshua <condorenajoshua98@gmail.com>
2025-08-04 20:39:52 -0500

    Se mejoro el diseño del login para telefono

--- my-app/app/Login/app.js
+++ my-app/app/Login/app.js
@@ -4,11 +4,14 @@ import {Image, ImageBackground, Pressable, StatusBar, StyleSheet, Text, TextInpu
 import usuarios from '../../hooks/usuarios'
 import { Link, Stack, useFocusEffect, useRouter } from 'expo-router'
 import { useUser } from '../../components/UserContext'
-import portada from "../../assets/portada1.png"
-import imagen_principal from "../../assets/imagen1.png"
+
 import login from "../../assets/login1.png"
-import ramas from "../../assets/ramam.png"
-import fondo from "../../assets/fondo_flora.png"
+import ramas from "../../assets/ramas4.png"
+
+import fondo2 from "../../assets/fondoprueba.png"
+import fondov2 from "../../assets/fondov2.png" 
+
+import { IconEntrada } from '../../assets/Icons'
 export default function AppPrincipal() {
     const {setUser}=useUser()
   const {FectUsuarios,dataUser}=usuarios()
@@ -30,104 +33,42 @@ export default function AppPrincipal() {
             navegar.push("/Login")
         }
     }
-    const Nube=({top,left,size=80,ancho=80,color="pink"})=>(
-        <View style={{
-        position:"absolute",
-        top:top,
-        left:left,
-        height:size,
-        width:ancho,
-        backgroundColor:color,
-        borderRadius:99  
-        }}>
-            
-        </View>
-    )
-    const LadyFlor=({abajo,lef,arriba="",isq=""})=>{
-       return(
-         <View style={{position:"absolute",
-            bottom:abajo,
-            left:lef,
-            top:arriba,
-            right:isq
-        }}>
-            <Image source={portada} alt='nose encuentra' style={{width:100,height:100}}></Image>
-        </View>
-       )
-    }
     const LoginLogo=()=>{
         return(
-            <Image source={login} style={{width:120,height:120,borderRadius:20}}></Image>
+            <Image source={login} style={{width:100,height:100,borderRadius:20}}></Image>
         )
     }
-    const ConjuntoFlores=({arriba,abajo=0,lef})=>{
-        return(
-            <View style={{position:"absolute",bottom:abajo,top:arriba,left:lef}}>
-                <Image source={imagen_principal} style={{width:100,height:100,borderRadius:20}}></Image>
-            </View>
-        )
-    }
-    const Ramas=()=>{
-        return(
-            <Image source={ramas} style={{width:250,height:200}}></Image>
-        )
-    } 
+    
   return (
    <>
    
    
    <Stack.Screen options={{headerShown:false}}></Stack.Screen>
    <View style={{flex: 1,backgroundColor:"#807c76"}}>
-    <ImageBackground source={fondo} style={styles.fondo} resizeMode='stretch' imageStyle={{alignSelf:"flex-end"}} >
-    {/*
-    <View style={{position:"absolute",top:30,left:-20}}>
-   
-     <Nube top={50} size={80} left={150}></Nube>
-     <Nube top={20} size={110} left={100}></Nube>
-     <Nube top={50} size={80} left={50}></Nube>
-    </View>
-  <View style={{position:"absolute",bottom:250,left:160}}>
-     <Nube top={50} size={30} ancho={40} left={120}></Nube>
-     <Nube top={30} size={50} ancho={40} left={100}></Nube>
-     <Nube top={50} size={30} ancho={40} left={80}></Nube>
-  </View>
-  <View style={{position:"absolute",top:300,left:10}}>
-    <Image source={ramas} style={{width:`${60}`,height:`${60}`}}></Image>    
-  </View>
- <LadyFlor abajo={0} lef={10}></LadyFlor>
- <LadyFlor abajo={0} isq={0} arriba={0}></LadyFlor>
-    <ConjuntoFlores></ConjuntoFlores>
-    <View style={{position:"absolute",top:0,left:170}}>
-    <Ramas></Ramas>
-   </View>
-   <View style={{position:"absolute",bottom:0}}>
-    <Ramas></Ramas>
-   </View>
-   <View style={{position:"absolute",bottom:0,left:200}}>
-    <Ramas></Ramas>
-   </View>
-    */}
-    
+    <ImageBackground source={fondov2} style={styles.fondo2}></ImageBackground>
+    <ImageBackground source={fondo2} style={styles.fondo} resizeMode='contain' imageStyle={{alignSelf:"flex-end"}} >
+        
     <View style={styles.contenedor}>
-    <View style={{alignItems:"center"}}>
+    <View style={{position:"absolute",top:180,left:100,alignItems:"center"}}>
         <LoginLogo></LoginLogo>    
     </View> 
-    <View className='m-4'>
+    <View style={{position:"absolute",top:260,left:0,width:280}} className='m-4'>
         <Text >Correo</Text>
         <TextInput value={correo} onChangeText={setcorreo} className='rounded w-full bg-gray border-2' placeholder='example@gmail.com'></TextInput>
     </View>
-    <View className=' m-4'>
+    <View style={{position:"absolute",top:340,left:0,width:280}} className='m-4'>
         <Text>Password</Text>
         <TextInput value={pass} onChangeText={setpass} className='rounded w-full bg-gray border-2' placeholder='*********'></TextInput>
     </View>
-    <View className='m-4 '>
-        <Link className='text-right' href={"/Perfil/create"}>No tienes cuenta?</Link>
+    <View  style={{position:"absolute",top:440,left:5}} className='m-4 flex-row'>
+        <Link style={styles.cuenta} href={"/Perfil/create"}>No tienes cuenta?</Link>
     </View>
-    <View className='flex-row justify-center p-4'>
-        <Pressable onPress={VeificarUse} className='bg-blue-500  rounded-full p-4'>
-            <Text  className='color-white text-lg '> Enviar datos</Text>
+    <View style={{position:"absolute",bottom:250,left:120}} className='flex-row justify-center p-4'>
+        <Pressable onPress={VeificarUse} className='bg-white rounded-full '>
+            <IconEntrada></IconEntrada>
         </Pressable>
     </View>
+    
     </View>
     </ImageBackground>
   
@@ -137,10 +78,22 @@ export default function AppPrincipal() {
 )}
 //
 const styles=StyleSheet.create({
+    cuenta:{
+        textDecorationLine:"underline",
+        fontWeight:"700"
+    },
+    fondo2:{
+      
+      position: 'absolute',
+      opacity:0.7,
+      width: '100%',
+      height: '100%',
+        backgroundColor:"#625f59"
+    },
     fondo: {
-       
-    position: 'absolute',
-      bottom: -0,
+      
+      position: 'absolute',
+        
       width: '100%',
       height: '100%',
     
