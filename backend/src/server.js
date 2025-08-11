cee8ef65d2810ea5bc8adf98a46a9a7d07e9fc1f
joshua <condorenajoshua98@gmail.com>
2025-08-10 19:53:22 -0500

    Se modifico la dirrecion ip del server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
 //const sequelize = require('sequelize')
 const app=require('./app')
+const path=require('path')
+const express=require('express')
 const router=require('./routes/useroutes')
+// ⚠️ IMPORTANTE: debe ir antes de app.use("/",router)
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use("/",router)
 const port=process.env.port|| 4000
-ruta_app="192.168.18.24"
+ruta_app="192.168.18.20"
+
 app.listen(port,()=>{
     console.log(`Entra al siguiente puerto: http://${ruta_app}:${port}`)
     console.log(process.env.DB_NAME)
