b7adb97a2ad2e2d52e42d702a1cf95c3b749dd89
joshua <condorenajoshua98@gmail.com>
2025-08-14 19:39:28 -0500

    Se creo un helper para habitos renderizar imagenes

--- my-app/hooks/Actividades.js
+++ my-app/hooks/Actividades.js
@@ -2,13 +2,18 @@ import axios from 'axios'
 import React, { useState } from 'react'
 import Constants from "expo-constants"
 import { useUser } from '../components/UserContext'
+import GetImage from '../utils/GetImage'
 export default function Actividades() {
   const {user}=useUser()
   const host=Constants.expoConfig.extra.host
   const [actividades, setactividades] = useState([])
   const FetchActividades=async()=>{
     const {data}=await axios.get(`http://${host}:4000/actividades/${user.id}`)
-    setactividades(data)
+    const actiUrl=data.map((a)=>({
+      ...a,
+      imagen:GetImage(a.imagen)
+    }))
+    setactividades(actiUrl)
   }
     return {FetchActividades,actividades}
    
