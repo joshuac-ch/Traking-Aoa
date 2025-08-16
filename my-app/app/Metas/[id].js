02d2545a8ed0d589d049137ef583da3c84980771
joshua <condorenajoshua98@gmail.com>
2025-08-15 19:39:31 -0500

    Se implemento cambios en el form

--- my-app/app/Metas/[id].js
+++ my-app/app/Metas/[id].js
@@ -1,11 +1,12 @@
 import axios from 'axios'
 import { Stack, useLocalSearchParams, useRouter } from 'expo-router'
 import React, { useEffect, useState } from 'react'
-import { Button, Image, Pressable, StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, Button, Image, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
 import constantes from 'expo-constants'
 import { TextInput } from 'react-native'
 import { DateTimePickerAndroid } from '@react-native-community/datetimepicker'
 import * as ImagePicket from "expo-image-picker"
+import GetImage from '../../utils/GetImage'
 export default function DetalleMetas() {
   const {id}=useLocalSearchParams()
   const navegar=useRouter()
@@ -28,20 +29,22 @@ export default function DetalleMetas() {
             descripcion:data.descripcion,
             proceso:data.proceso,
             meta_total:data.meta_total.toString(),
-            imagen:data.imagen,
+            imagen:GetImage(data.imagen),            
             fecha_limite:new Date(data.fecha_limite)
         })        
     }catch(err){
         alert("Error: "+ err.message)
     }    
 }
+    const [loadding, setloadding] = useState(false)
     const UpdateMeta=async()=>{
+        setloadding(true)
         let imageUrl=""
         if(FormDataMetas.imagen.startsWith("file://")){
             const newDataMeta=new FormData()
             newDataMeta.append("imagen",{
                 uri:FormDataMetas.imagen,
-                name:"update-metas",
+                name:"update-metas.png",
                 type:"image/jpeg"
             })
             const response=await axios.post(`http://${host}:4000/upload`,newDataMeta,{headers:{"Content-Type":"multipart/form-data"}})
@@ -57,6 +60,7 @@ export default function DetalleMetas() {
             })
             alert("Se actualizaron los datos exitosamente")
             //navegar.push("/Panel")    
+            setloadding(false)
         }catch(err){
             alert("Hubo un error al enviar los datos: ",err.response?.data?.message )
         }
@@ -90,7 +94,9 @@ export default function DetalleMetas() {
   return (
     <>
     <Stack.Screen options={{title:`Meta N°${id}`}}></Stack.Screen>
-    <View>
+    <ScrollView>
+        {
+        <View style={{marginTop:20,marginBottom:30}}>
        {FormDataMetas!=null?
        <View className='m-4'>
             <View>
@@ -123,17 +129,35 @@ export default function DetalleMetas() {
 
             </View>
             <View>
-                <Pressable onPress={()=>UpdateMeta()}>
-                    <Text>Actualizar Meta</Text>
-                </Pressable>    
+                {
+                    loadding?
+                    <View style={styles.btn_enviar}>
+                        <ActivityIndicator></ActivityIndicator>
+                    </View>
+                    :
+                    <Pressable style={styles.btn_enviar} onPress={()=>UpdateMeta()}>
+                        <Text style={{textAlign:"center",color:"white"}}>Actualizar Meta</Text>
+                    </Pressable>    
+                }
             </View>  
        </View>
        :<Text>No hay datos disponibles</Text>}
-    </View>    
+    </View>
+    }    
+    </ScrollView>    
     </>
   )
 }
 const styles=StyleSheet.create({
+    btn_enviar:{
+        marginTop:20,
+        backgroundColor:"purple",
+        padding:10,
+        borderRadius:20,
+        width:200,
+        alignSelf:"center",
+        
+    },
     form_input:{
         borderRadius:10,
         borderWidth:2,
