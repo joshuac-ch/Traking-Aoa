a3b8778e8549e0e9ac874f977bad9f083e0c32c3
joshua <condorenajoshua98@gmail.com>
2025-08-17 19:42:39 -0500

    Se agrego el comopenete Prev para imagenes

---
+++ my-app/components/PubicacionPrev.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react'
+import { Image, Modal, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { IconElipsis, IconHeart, IconReply } from '../assets/Icons'
+import Comentario from './Comentario'
+import GetImage from '../utils/GetImage'
+export default function PubicacionPrev({datosUser,datasRutina,publicacionID}) {
+const [imagenExpandida, setimagenExpandida] = useState(false)    
+  return (
+    <ScrollView>
+        <View style={styles.contenedor_principal}>
+            <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center',margin:10}}>
+                <View style={{display:'flex',flexDirection:'row'}}>
+                    <View >
+                        <Image source={{uri:GetImage(datosUser.imagen)}} style={{width:60,height:60,borderRadius:10,marginRight:10}}></Image>
+                    </View>
+                    <View>
+                        <Text style={{fontWeight:'bold'}}>Creador</Text>
+                        <View style={{flexDirection:'row'}}>
+                            <Text style={{marginRight:20}}>{datosUser.nombre}</Text>
+                            <Text style={{color:'#75706f'}}>{datasRutina.fecha_inicio?new Date(datasRutina.fecha_inicio).toLocaleDateString():""}</Text>
+                        </View>
+                        <Text>{datosUser.correo}</Text>
+                    </View>        
+                </View>
+                <View>
+                    <IconElipsis></IconElipsis>
+                </View>
+            </View>
+    
+            <View >
+                <View style={{padding:10}}>
+                    <Text>{datasRutina.titulo}</Text>
+                    <Text>{datasRutina.descripcion}</Text>
+                </View>        
+                <View>
+                    <Pressable onPress={()=>setimagenExpandida(true)}>
+                         <Image style={styles.imagen} source={{ uri: datasRutina.imagen }} />
+                    </Pressable>
+                    <Modal visible={imagenExpandida} animationType="fade">
+                        <View style={styles.modal}>
+                        <Pressable style={styles.zona} onPress={()=>setimagenExpandida(false)}>
+                             <Image style={styles.imagen_expandida} source={{ uri:datasRutina.imagen }} />
+                        </Pressable>
+                        </View>                    
+                    </Modal>             
+                
+               
+                </View>
+                <View style={{flexDirection:"row",justifyContent:'space-around',alignItems:'center'}}>
+                    <View  style={{flexDirection:'row',margin:10,alignItems:'center'}}>
+                        <IconHeart></IconHeart>
+                        <Text style={{marginLeft:10}}>Love</Text>
+                    </View>
+                    <View style={{flexDirection:'row',margin:10,alignItems:'center'}}>
+                        <IconReply></IconReply>
+                        <Text style={{marginLeft:10}}>Compartir</Text>
+                    </View>
+                </View>
+                <View>
+                    <Comentario pubID={publicacionID} estado={false}></Comentario>
+                </View>
+            </View>
+        </View>
+    </ScrollView>
+  )
+}
+const styles=StyleSheet.create({
+    contenedor_principal:{
+        marginLeft:20,
+        marginRight:20,
+        marginTop:20,
+        marginBottom:40,
+        backgroundColor:"white",
+        borderRadius:20,
+        borderWidth:2,
+        borderStyle:'solid',
+        borderColor:"transparent"
+    },
+    zona:{
+       flex:1,
+    },
+    modal:{
+        flex:1,
+        backgroundColor:"black",
+    },
+    imagen:{
+        margin:10,
+        width:250,
+        height:300,
+        alignSelf:'center',
+        borderRadius:20,        
+    },
+    imagen_expandida:{
+        width:"100%",
+        height:"100%",
+        resizeMode:"contain"
+    },
+})
