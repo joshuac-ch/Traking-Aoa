bc8c509670b9f3ad4e11ae651292d57d5d188373
joshua <condorenajoshua98@gmail.com>
2025-09-02 20:00:14 -0500

    Ultimos cambios

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,8 +2,12 @@ const  {Sequelize}= require("sequelize")
 const  dotenv = require("dotenv")
 dotenv.config()
 // Configuración de la base de datos
-module.exports = sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+module.exports = sequelize = new Sequelize(
+    process.env.DB_NAME||"track", 
+    process.env.DB_USER||"root",
+    process.env.DB_PASS||"admin123", {
+    host: process.env.DB_HOST||"127.0.0.1",
+    port: process.env.DB_PORT || 3306,
     dialect: "mysql",
     logging: false, // Desactiva logs de SQL   
 });
