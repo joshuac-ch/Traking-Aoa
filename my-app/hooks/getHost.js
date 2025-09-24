2ffc1911e76f035d65aaef7327ac5d3006b9678b
joshua <condorenajoshua98@gmail.com>
2025-09-23 20:02:28 -0500

    Modificacion internas

--- my-app/hooks/getHost.js
+++ my-app/hooks/getHost.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import constantes from "expo-constants"
 export default function getHost() {
-    return(constantes?.expoConfig?.extra?.host|| "192.168.18.25")// fallback hardcodeado opcional
+    return(constantes?.expoConfig?.extra?.host|| "3.87.87.124")// fallback hardcodeado opcional
      
 }
